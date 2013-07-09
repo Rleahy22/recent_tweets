6a03002313332917f3b9e42fb6de23c355efd773
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-09 09:27:02 -0500

    Completed

--- public/js/application.js
+++ public/js/application.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-  $('.loading').show();
+  $('.loading').hide();
   $('#tweeter_form').on('submit', function(event) {
+    $('p').empty();
     event.preventDefault();
-    //console.log($('input[name="tweeter"]').val());
+    $('.loading').show();
     $.get('/' + $('input[name="tweeter"]').val(), function(tweetData) {
-      $('.loading').show();
+      $('.loading').hide();
       $('.tweets').html(tweetData);
     });
-    //showLoader = setTimeout("$('.loading').show()", 300);
   });
-  if ($('#load_wait').length === 0){
-    $('.loading').show();
-  } else {
-    $('.loading').show();
-  };
 });
-
-
-
-
